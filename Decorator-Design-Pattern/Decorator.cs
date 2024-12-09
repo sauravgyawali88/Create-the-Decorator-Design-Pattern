793837892048787d83d873b032baa2b452d6ad97
sauravgyawali88 <gyawalisaurav892@gmail.com>
2024-12-09 04:09:31 +0000

    Creating Decorator.cs and doing code

---
+++ Decorator-Design-Pattern/Decorator.cs
@@ -0,0 +1,19 @@
+// Decorator.cs
+// Abstract class that implements IComponent and holds a reference to a component to decorate it.
+public abstract class Decorator<T> : IComponent<T>
+{
+    // A field to hold the reference to the original component.
+    protected IComponent<T> component;
+
+    // Constructor to initialize the decorator with the component it decorates.
+    public Decorator(IComponent<T> component)
+    {
+        this.component = component;
+    }
+
+    // Override the GetText method to call the decorated component's GetText method.
+    public virtual T GetText()
+    {
+        return component.GetText();
+    }
+}
